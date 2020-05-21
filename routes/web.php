2f3e948e667a4f836150733ac801779961b5fb0e
root <root@localhost.localdomain>
2020-05-21 12:24:18 +0700

    updated

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,14 @@ Route::get('/greeting/{name?}', function ($name = null) {
     }
 
 });
+Route::get('/login', function () {
+    return view('login');
+});
+Route::post('/login', function (Illuminate\Http\Request $request) {
+    if ($request->username == 'admin'
+        && $request->password == 'admin') {
+        return view('welcome_admin');
+    }
+
+    return view('login_error');
+});
